cd440032338bcf0164633a05d267e883e6080f5a
fms12 <mayank2001.iimt@gmail.com>
2023-08-17 03:17:14 +0530

    ready for deployment

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyA4cy3tXrCFIVoTlQM1yCnBQfnbU5xxKns",
+  authDomain: "my-tube-55ffd.firebaseapp.com",
+  projectId: "my-tube-55ffd",
+  storageBucket: "my-tube-55ffd.appspot.com",
+  messagingSenderId: "726093277612",
+  appId: "1:726093277612:web:a6350775920c12d77ef209",
+};
