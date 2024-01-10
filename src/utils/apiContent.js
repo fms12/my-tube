618c4e2a3b05199a7c87991b9eea5b61fbe153ff
Mayank Singh <68012074+fms12@users.noreply.github.com>
2024-01-10 21:07:22 +0530

    Update apiContent.js

--- src/utils/apiContent.js
+++ src/utils/apiContent.js
@@ -2,10 +2,10 @@
 
 
 export const YOUTUBE_VIDEOS_API =
-  "https://corsproxy.org/?https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=" +
+  "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=" +
   process.env.REACT_APP_API_KEY;
 
 export const YOUTUBE_SEARCH_API =
-  "https://corsproxy.org/?https://clients1.google.com/complete/search?client=firefox&ds=yt&q=";
+  "https://clients1.google.com/complete/search?client=firefox&ds=yt&q=";
 
 export const LIVE_CHAT_COUNT =10;
